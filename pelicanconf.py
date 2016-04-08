df24c6b2d3795ca52019219fd601bdac32a3b339
Jacob Ritchie <jacob.w.k.ritchie@gmail.com>
2016-04-07 22:02:06 -0700

    Add output submodule, change theme, change settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Jacob Ritchie'
-SITENAME = u'Jacob Ritchie'
+SITENAME = u'Jacob\'s Larder'
 SITEURL = ''
 
 PATH = 'content'
@@ -11,6 +11,7 @@ PATH = 'content'
 TIMEZONE = 'EST'
 
 DEFAULT_LANG = u'en'
+LOCALE= u"en_GB"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,17 +20,31 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+'''
+Disabled by mg theme - may re-enable later 
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
+'''
 
-DEFAULT_PAGINATION = False
+SOCIAL = (('twitter', 'https://twitter.com/JWKRitchie'),
+          ('github', 'https://github.com/jritch'),
+          ('envelope', 'mailto:jacob.w.k.ritchie@gmail.com'),)
+
+
+#Parameters inherited from the mg-theme
+ALTNAME=SITENAME
+SITESUBTITLE="Dried and pickled musings"
+DESCRIPTION="A blog about whatever makes it out of the hole."
+
+#DEFAULT_PAGINATION = True
+
+THEME = "agit"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
